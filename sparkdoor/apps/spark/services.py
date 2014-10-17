677358f6a744e4ca73235300c6a38328b19fa9cd
Ben <dummerbd@appstate.edu>
2014-10-17 14:53:13 -0400

    Login method for SparkCloud service

--- sparkdoor/apps/spark/services.py
+++ sparkdoor/apps/spark/services.py
@@ -1,3 +1,43 @@
 """
 services.py - module for interacting with a Spark cloud service.
 """
+from hammock import Hammock
+
+from .settings import SparkSettings
+
+
+class SparkCloud:
+    """
+    Interface to interacting with a Spark cloud web service.
+    """
+    def __init__(self, access_token=None):
+        """
+        Constructer - instances a new web service using the path in
+        `SparkSettings.API_URI`. An `access_token` can be specified if
+        one has already been granted.
+        """
+        self._settings = SparkSettings()
+        self._service = Hammock(self._settings.API_URI)
+        self.access_token = access_token
+
+    def login(self, username=None, password=None):
+        """
+        Will attempt to get a new access_token from the cloud service
+        using `SparkSettings.USERNAME` and `SparkSettings.PASSWORD` or
+        the passed in `username` and `password`.
+
+        An invalid login will return None, otherwise the access token is
+        returned, which also sets the `access_token` attribute.
+        """
+        username = username or self._settings.USERNAME
+        password = password or self._settings.PASSWORD
+        auth = ('spark', 'spark')
+        data = {
+            'grant_type': 'password',
+            'username': username,
+            'password': password
+        }
+        response = self._service.oauth.token.POST(auth=auth, data=data)
+        if response.ok:
+            self.access_token = response.json().get('access_token')
+        return self.access_token
--- sparkdoor/apps/spark/tests/test_services.py
+++ sparkdoor/apps/spark/tests/test_services.py
@@ -1,3 +1,42 @@
 """
 test_serives.py - unit tests for the `spark` app's services module.
 """
+from django.test import SimpleTestCase, override_settings
+
+from sparkdoor.libs.httmock import HTTMock
+
+from .mocks import spark_cloud_mock
+from ..services import SparkCloud
+
+
+spark_test_settings = {
+    'CLOUD_USERNAME': 'a_user',
+    'CLOUD_PASSWORD': 'password',
+    'CLOUD_API_URI': 'https://api.test.com'
+}
+
+@override_settings(SPARK=spark_test_settings)
+class SparkCloudTestCase(SimpleTestCase):
+    """
+    Test case for `services.SparkCloud`.
+    """
+    def test_login_with_valid_credentials(self):
+        """
+        Test that `login` returns an access token and sets the
+        `access_token` attribute after a successful login.
+        """
+        with HTTMock(spark_cloud_mock):
+            cloud = SparkCloud()
+            token = cloud.login()
+        self.assertIsNotNone(token)
+        self.assertEqual(token, cloud.access_token)
+
+    def test_login_with_invalid_credentials(self):
+        """
+        Test that `login` returns None with invalid credentials.
+        """
+        with HTTMock(spark_cloud_mock):
+            cloud = SparkCloud()
+            token = cloud.login('not_a_valid_user', 'password')
+        self.assertIsNone(token)
+        self.assertIsNone(cloud.access_token)

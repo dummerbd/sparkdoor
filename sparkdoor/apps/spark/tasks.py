fc96cc95e90c978da3808c7936dbb4a42c30ba26
Ben <dummerbd@appstate.edu>
2014-10-19 18:55:40 -0400

    Added task to refresh access tokens

--- sparkdoor/apps/spark/tasks.py
+++ sparkdoor/apps/spark/tasks.py
@@ -1,3 +1,19 @@
 """
 tasks.py - `spark` app web worker tasks module.
 """
+from celery import shared_task
+
+from .models import CloudCredentials
+
+
+@shared_task
+def refresh_access_token():
+    """
+    Run this task periodically to check for soon to expire access tokens
+    and request a new one when needed. This task can also be run on
+    demand in the case that an access token is expired.
+    """
+    token = CloudCredentials.objects.access_token()
+    if token is None:
+        token = CloudCredentials.objects.renew_token()
+    return token
--- sparkdoor/apps/spark/tests/test_tasks.py
+++ sparkdoor/apps/spark/tests/test_tasks.py
@@ -1,6 +1,63 @@
 """
 test_tasks.py - test cases for the `spark` app's tasks module.
 """
-from django.test import TestCase, override_settings
+from datetime import timedelta
 
-from httmock import HTTMock
+from django.test import TestCase, override_settings
+from django.utils import timezone
+
+from sparkdoor.libs.httmock import HTTMock
+
+from .mocks import spark_cloud_mock, ACCESS_TOKEN
+from .factories import CloudCredentialsFactory
+from ..tasks import refresh_access_token
+from ..models import CloudCredentials
+
+
+spark_test_settings = {
+    'CLOUD_USERNAME': 'a_user',
+    'CLOUD_PASSWORD': 'password',
+    'CLOUD_API_URI': 'https://api.test.com'
+}
+
+
+@override_settings(SPARK=spark_test_settings)
+class RefreshAccessTokenTestCase(TestCase):
+    """
+    Test case for `tasks.refresh_access_token`.
+    """
+    factory = CloudCredentialsFactory
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Add some test dates.
+        """
+        now = timezone.now()
+        cls.expired_dt = now + timedelta(days=-10)
+        cls.current_dt = now + timedelta(days=90)
+
+    def test_returns_good_token(self):
+        """
+        If there is a good token (not expired) then don't do anything,
+        just return that token.
+        """
+        self.assertEqual(CloudCredentials.objects.count(), 0)
+        cred = self.factory.create(access_token='good_token', expires_at=self.current_dt)
+        with HTTMock(spark_cloud_mock):
+            token = refresh_access_token()
+        self.assertEqual(CloudCredentials.objects.count(), 1)
+        self.assertEqual(token, 'good_token')
+        cred.delete()
+
+    def test_renews_token(self):
+        """
+        If there isn't any good tokens then renew one.
+        """
+        self.assertEqual(CloudCredentials.objects.count(), 0)
+        cred = self.factory.create(access_token='old_token', expires_at=self.expired_dt)
+        with HTTMock(spark_cloud_mock):
+            token = refresh_access_token()
+        self.assertEqual(CloudCredentials.objects.count(), 2)
+        self.assertEqual(token, ACCESS_TOKEN)
+        cred.delete()

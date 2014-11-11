3db86fa560cbe6e09939441f89e8a76346ccf96c
Ben <dummerbd@appstate.edu>
2014-11-11 13:27:13 -0500

    Added devices list

--- sparkdoor/apps/common/tests/test_views.py
+++ sparkdoor/apps/common/tests/test_views.py
@@ -1,9 +1,12 @@
 """
 test_views.py - test cases for the `common` app's view module.
 """
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
 
 from sparkdoor.libs.testmixins import ViewsTestMixin
+from sparkdoor.libs.factories import UserFactory
+from sparkdoor.apps.spark.models import Device
+from sparkdoor.apps.spark.tests.factories import DeviceFactory
 
 from .. import views
 
@@ -19,3 +22,28 @@ class HomeViewTestCase(ViewsTestMixin, SimpleTestCase):
         Test that the correct template is used.
         """
         self.assertCorrectTemplateUsed('common/home.html')
+
+class DevicesViewTestCase(ViewsTestMixin, TestCase):
+    """
+    Test case for `views.DevicesView`.
+    """
+    view_class = views.DevicesView
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Add test devices.
+        """
+        cls.user = UserFactory.create()
+        cls.devices = [DeviceFactory.create(user=cls.user) for _ in range(3)]
+        print(cls.devices)
+
+    def test_context_data(self):
+        """
+        Test that the `context` includes a `devices` entry.
+        """
+        response = self.send_request_to_view()
+        context = response.context_data
+        expected_devices = Device.objects.for_user(self.user).order_by('name')
+        for i, device in enumerate(expected_devices):
+            self.assertEqual(device.id, context['devices'][i].id)
--- sparkdoor/apps/common/views.py
+++ sparkdoor/apps/common/views.py
@@ -2,8 +2,12 @@
 views.py - `common` app views module.
 """
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
 
 from braces.views import LoginRequiredMixin
+
+from sparkdoor.apps.spark.forms import RegisterDeviceForm
+from sparkdoor.apps.spark.models import Device
 
 
 class HomeView(TemplateView):
@@ -13,16 +17,18 @@ class HomeView(TemplateView):
     template_name = 'common/home.html'
 
 
-class DevicesView(LoginRequiredMixin, TemplateView):
+class DevicesView(LoginRequiredMixin, CreateView):
     """
     Serves as a user profile page, provides a list of devices for the
     user.
     """
     template_name = 'common/devices.html'
+    form_class = RegisterDeviceForm
 
     def get_context_data(self, **kwargs):
         """
         Add a `devices` entry with available devices.
         """
         context = super(DevicesView, self).get_context_data(**kwargs)
+        context['devices'] = Device.objects.for_user(self.request.user).order_by('name')
         return context

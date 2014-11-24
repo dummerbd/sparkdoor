1d742de59eb6952b219a3f76abef80eca305d134
Adam Van Hine <vanhineam@appstate.edu>
2014-11-24 11:46:31 -0500

    Added QR code reading on the device page

--- sparkdoor/apps/common/urls.py
+++ sparkdoor/apps/common/urls.py
@@ -13,6 +13,10 @@ urlpatterns = patterns('',
         name='profile'
     ),
 
+    url(r'^qrcode$',
+    	views.QrcodeView.as_view(),
+    	name='qrcode'
+    ),
     # this needs to be last:
     url(r'^$',
         views.HomeView.as_view(),
